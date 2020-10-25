2b639440aef853d91db1bbdb7918de0164f13e7e
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-10-25 13:35:10 -0300

    Adding support for the H2 database

--- photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/controller/UserController.java
+++ photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.gabriel.photoappusersservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final Environment env;
+    
     @GetMapping
     public String status() {
-        return "enabled";
+        return "enabled on port " + env.getProperty("local.server.port");
     }
 }

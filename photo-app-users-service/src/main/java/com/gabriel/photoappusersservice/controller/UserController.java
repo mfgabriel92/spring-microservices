56a345ff581a3dc91d0b025b530b4b08fce3ec1a
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-10-24 21:17:01 -0300

    Create a user REST controller

---
+++ photo-app-users-service/src/main/java/com/gabriel/photoappusersservice/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.gabriel.photoappusersservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String status() {
+        return "enabled";
+    }
+}

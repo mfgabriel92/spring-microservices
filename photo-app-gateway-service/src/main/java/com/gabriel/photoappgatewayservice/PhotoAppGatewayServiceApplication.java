b294418ec932ec84b3a3354c9facc6c57fa10611
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-10-25 10:07:11 -0300

    Set up ZUUL gateway service

---
+++ photo-app-gateway-service/src/main/java/com/gabriel/photoappgatewayservice/PhotoAppGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.gabriel.photoappgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class PhotoAppGatewayServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppGatewayServiceApplication.class, args);
+	}
+}

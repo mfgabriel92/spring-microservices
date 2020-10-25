65be63294316876fde1f032ece0dca78545f7454
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-10-24 21:08:19 -0300

    Start up user service

---
+++ photo-app-users-service/src/main/java/com/gabriel/photoappusersservice/PhotoAppUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.photoappusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppUsersServiceApplication.class, args);
+	}
+
+}

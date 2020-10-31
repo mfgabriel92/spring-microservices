f19bb1fa8174fb511e4dc562597512f95f8ae02e
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2020-10-31 13:53:01 -0300

    Added Spring security and password encryption

--- photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/config/ModelMapperConfig.java
+++ photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/config/ModelMapperConfig.java
@@ -3,9 +3,9 @@ package com.gabriel.photoappusersservice.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
--- photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/service/UserService.java
+++ photo-app-user-service/src/main/java/com/gabriel/photoappusersservice/service/UserService.java
@@ -3,14 +3,20 @@ package com.gabriel.photoappusersservice.service;
 import com.gabriel.photoappusersservice.model.User;
 import com.gabriel.photoappusersservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
     
+    private final PasswordEncoder passwordEncoder;
+    
+    @Transactional
     public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 }
